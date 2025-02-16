a6d8b0a8e50b4698c36c4126761f84d79b9804a2
harrythe6arry <voravit.sri@student.mahidol.edu>
2025-02-16 14:13:13 +0700

    fix: change the tests to support the new methods

--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -17,10 +17,9 @@ class StudentsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create student" do
-    assert_difference("Student.count") do
-      post students_url, params: { student: { name: "newstudent" } }
+    assert_difference("Student.count", 1) do
+      post students_url, params: { student: { name: "newstudent", section: "A", grade: "10" } }
     end
-
     assert_redirected_to student_url(Student.last)
   end
 
@@ -36,15 +35,27 @@ class StudentsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update student" do
-    patch student_url(@student), params: { student: { name: "newname" } }
+    patch student_url(@student), params: { student: { name: "newname", is_active: 1, section: "B", grade: "10" } }
     assert_redirected_to student_url(@student)
   end
 
-  test "should destroy student" do
-    assert_difference("Student.kept.count", -1) do
-      delete student_url(@student)
-    end
 
-    assert_redirected_to students_url
+  test "archive student after deletion" do
+
+    delete student_url(@student)
+    @student.reload
+
+    # Assert that the student is archived (is_active should be true)
+    assert_equal true, @student.is_active, "Student was not archived"
+
+    # Assert the correct redirect after the action
+    assert_redirected_to students_path
+
+    # Assert the flash message is correct
+    assert_equal "#{@student.name} was archived successfully.", flash[:notice]
   end
+
+
+
+
 end
