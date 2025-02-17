5b672af845acb1aaa76ceea80c8592cc1d27482e
chadakeng <chada.keng@gmail.com>
2025-02-17 19:41:44 +0700

    Feat: Added qr generator that adds hash as well and replaced the old qr in the view area.

---
+++ app/helpers/qr_helper.rb
@@ -0,0 +1,20 @@
+module QrHelper
+  require 'rqrcode'
+  require 'digest'
+
+  SECRET_KEY = Rails.application.credentials.secret_key_base
+
+  def generate_qr_code(uid)
+    security_hash = Digest::SHA256.hexdigest("#{uid}|#{SECRET_KEY}")
+    qr_data = "#{uid},#{security_hash}"
+
+    qr = RQRCode::QRCode.new(qr_data)
+    qr.as_svg(
+      offset: 0,
+      color: '000',
+      shape_rendering: 'crispEdges',
+      module_size: 6,
+      standalone: true
+    ).html_safe
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,25 @@
 Rails.application.routes.draw do
   resources :attendances
-  resources :students
+   resources :students do
+    collection do
+      post 'mark_attendance'
+    end
+  end
   get "home/index"
   resource :session
   resources :passwords, param: :token
   resources :signup, only: %i[new create]
-  resources :students
   resources :users, only: %i[index] do
     member do
       patch :approve
       delete :cancel
     end
   end
+
+
+  get "/admin/scan_qr", to: "admin#scan_qr"
+  post "/admin/checkin", to: "admin#checkin"
+  get "/students/scan", to: "admin#scan_qr"
   get "up" => "rails/health#show", as: :rails_health_check
   root "home#index"
 end
