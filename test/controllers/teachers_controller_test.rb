2575f9c8321562c276a67e036d0441c00011d365
chadakeng <chada.keng@gmail.com>
2025-02-24 12:48:57 +0700

    Fix: Changed params of teachers_controller_test.rb to reflect the new database structure

--- test/controllers/teachers_controller_test.rb
+++ test/controllers/teachers_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class TeachersControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @user = User.create!(email_address: "admin@test.com", password: "password123", role: "admin", approved: true, school_id: 1)
+    @user = User.create!(email_address: "admin@test.com", password: "password123", role: "admin", approved: true, school_id: 1, personal_email: "admin@personal", first_name: "Admin", last_name: "User")
     post session_path, params: { email_address: @user.email_address, password: "password123" }
   end
 
@@ -12,7 +12,7 @@ class TeachersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should destroy teacher" do
-    teacher = User.create!(email_address: "teacher@test.com", password: "password123", role: "teacher", approved: true, school_id: @user.school_id)
+    teacher = User.create!(email_address: "teacher@test.com", password: "password123", role: "teacher", approved: true, school_id: @user.school_id, personal_email: "teacher@personal", first_name: "Teacher", last_name: "User")
     assert_difference("User.count", -1) do
       delete teacher_path(teacher)
     end
