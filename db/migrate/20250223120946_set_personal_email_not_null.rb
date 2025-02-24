f3d7e5527c55d6ef602a00cdc309e8f94d40b144
chadakeng <chada.keng@gmail.com>
2025-02-24 14:15:34 +0700

    Fix: - Fix NOT NULL constraint issue for personal_email

--- db/migrate/20250223120946_set_personal_email_not_null.rb
+++ db/migrate/20250223120946_set_personal_email_not_null.rb
@@ -1,5 +1,15 @@
 class SetPersonalEmailNotNull < ActiveRecord::Migration[8.0]
-  def change
+  def up
+    execute <<-SQL
+      UPDATE users
+      SET personal_email = email_address
+      WHERE personal_email IS NULL;
+    SQL
+
     change_column_null :users, :personal_email, false
+  end
+
+  def down
+    change_column_null :users, :personal_email, true
   end
 end
