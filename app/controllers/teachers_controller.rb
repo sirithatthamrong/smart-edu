21cc27ce3e5d0ca09642368f0fce2c648fc330c8
Ling <kanladaporn.sir@gmail.com>
2025-02-28 00:04:48 +0700

    feat(manage-teachers): add first name, last name, and personal email to table

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -3,6 +3,7 @@ class TeachersController < ApplicationController
 
   def index
     @teachers = User.where(role: "teacher", school_id: current_user.school_id, approved: true)
+                    .select(:id, :first_name, :last_name, :email_address, :personal_email)
   end
 
   def destroy
